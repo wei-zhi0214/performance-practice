0811431639c6d4fd9eb7db6a6b94853887c9bb61
william <xu06jo654@gmail.com>
2026-03-21 04:31:44 +0800

    finish 3.1

--- Homework/Homework_02/homework/main.c
+++ Homework/Homework_02/homework/main.c
@@ -62,7 +62,7 @@ int main(int argc, char** argv) {
   struct testFunc_t testFunc[] = {
     {&sort_a, "sort_a\t\t"},
     {&sort_a, "sort_a repeated\t"},
-    //{&sort_i, "sort_i\t\t"},
+    {&sort_i, "sort_i\t\t"},
     //{&sort_p, "sort_p\t\t"},
     //{&sort_c, "sort_c\t\t"},
     //{&sort_m, "sort_m\t\t"},
