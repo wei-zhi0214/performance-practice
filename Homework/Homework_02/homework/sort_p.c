cfd08de34208c8979aff2ef3dd2686fb64f0b4c5
william <xu06jo654@gmail.com>
2026-03-22 00:43:39 +0800

    finish 3.2

--- Homework/Homework_02/homework/sort_p.c
+++ Homework/Homework_02/homework/sort_p.c
@@ -23,7 +23,64 @@
 
 #include "./util.h"
 
+// Function prototypes
+static __attribute__((always_inline)) void merge_p(data_t* A, int p, int q, int r);
+static __attribute__((always_inline)) void copy_p(data_t* source, data_t* dest, int n);
+
+// A basic merge sort routine that sorts the subarray A[p..r]
 void sort_p(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+  assert(A);
+  if (p < r) {
+    int q = (p + r) / 2;
+    sort_p(A, p, q);
+    sort_p(A, q + 1, r);
+    merge_p(A, p, q, r);
+  }
 }
 
+// A merge routine. Merges the sub-arrays A [p..q] and A [q + 1..r].
+// Uses two arrays 'left' and 'right' in the merge operation.
+static __attribute__((always_inline)) void merge_p(data_t* A, int p, int q, int r) {
+  assert(A);
+  assert(p <= q);
+  assert((q + 1) <= r);
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  data_t* left = 0, * right = 0;
+  mem_alloc(&left, n1 + 1);
+  mem_alloc(&right, n2 + 1);
+  if (left == NULL || right == NULL) {
+    mem_free(&left);
+    mem_free(&right);
+    return;
+  }
+
+  copy_p(A + p, left, n1);
+  copy_p(A + q + 1, right, n2);
+  left[n1] = UINT_MAX;
+  right[n2] = UINT_MAX;
+
+  data_t* lptr = left;
+  data_t* rptr = right;
+  data_t* dst  = A + p;
+
+  for (int k = p; k <= r; k++) {
+    if (*lptr <= *rptr) {
+      *dst++ = *lptr++;
+    } else {
+      *dst++ = *rptr++;
+    }
+  }
+  mem_free(&left);
+  mem_free(&right);
+}
+
+static __attribute__((always_inline)) void copy_p(data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  for (int i = 0; i < n; i++) {
+    *dest++ = *source++;
+  }
+}
