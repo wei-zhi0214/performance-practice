590ecd69f6dbaffd9f7f5c499be694e570749aa8
william <xu06jo654@gmail.com>
2026-03-22 15:47:26 +0800

    finish 3.4 Reducing Memory Usage

--- Homework/Homework_02/homework/sort_m.c
+++ Homework/Homework_02/homework/sort_m.c
@@ -23,7 +23,92 @@
 
 #include "./util.h"
 
+// Function prototypes
+void isort(data_t* begin, data_t* end);
+static __attribute__((always_inline)) void merge_m(data_t* A, int p, int q, int r);
+static __attribute__((always_inline)) void copy_m(data_t* source, data_t* dest, int n);
+#define THRESHOLD 32
+
+// A basic merge sort routine that sorts the subarray A[p..r]
 void sort_m(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+  assert(A);
+  if (p < r) {
+    if (r - p < THRESHOLD) {
+      isort(A + p, A + r);
+      return;
+    }
+    int q = (p + r) / 2;
+    sort_m(A, p, q);
+    sort_m(A, q + 1, r);
+    merge_m(A, p, q, r);
+  }
 }
 
+// A merge routine. Merges the sub-arrays A[p..q] and A[q+1..r].
+// Copies only the SMALLER half to minimize allocation.
+//
+// Case 1: left half smaller (n1 <= n2)
+//   Copy left to temp, merge temp+A[q+1..r] forward into A[p..r].
+//   Write pointer k never overtakes right read pointer j (k <= j always).
+//
+// Case 2: right half smaller (n2 < n1)
+//   Copy right to temp, merge A[p..q]+temp backward into A[p..r].
+//   Write pointer k never overtakes left read pointer i (k >= i always).
+static __attribute__((always_inline)) void merge_m(data_t* A, int p, int q, int r) {
+  assert(A);
+  assert(p <= q);
+  assert((q + 1) <= r);
+  int n1 = q - p + 1;
+  int n2 = r - q;
+  int nsmall = (n1 <= n2) ? n1 : n2;
+
+  data_t* temp = NULL;
+  mem_alloc(&temp, nsmall + 1);
+  if (temp == NULL) {
+    mem_free(&temp);
+    return;
+  }
+
+  if (n1 <= n2) {
+    // Case 1: copy left half, merge forward
+    copy_m(A + p, temp, n1);
+    temp[n1] = UINT_MAX;  // sentinel
+
+    int i = 0;      // index into temp (left half copy)
+    int j = q + 1;  // index into right half in A
+
+    for (int k = p; k <= r; k++) {
+      if (j > r || temp[i] <= A[j]) {
+        A[k] = temp[i++];
+      } else {
+        A[k] = A[j++];
+      }
+    }
+  } else {
+    // Case 2: copy right half, merge backward
+    copy_m(A + q + 1, temp, n2);
+    temp[n2] = UINT_MAX;  // sentinel (used as lower bound guard below)
+
+    int i = q;      // index into left half in A (right end)
+    int j = n2 - 1; // index into temp (right half copy, right end)
+
+    for (int k = r; k >= p; k--) {
+      if (i < p || temp[j] >= A[i]) {
+        A[k] = temp[j--];
+      } else {
+        A[k] = A[i--];
+      }
+    }
+  }
+
+  mem_free(&temp);
+}
+
+static __attribute__((always_inline)) void copy_m(data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  for (int i = 0; i < n; i++) {
+    dest[i] = source[i];
+  }
+}
